3480409d00d2bad4ed31de85b630ab28745de9fa
HongDang94 <hong.thdang@gmail.com>
2022-07-27 23:42:23 +0700

    updtae

--- js/main.js
+++ js/main.js
@@ -14,18 +14,54 @@ jQuery(document).ready(function ($) {
     spaceBetween: 24,
   });
 
-  var swiperBlogSlider = new Swiper(".blogSlider", {
+  var swiperBlogSlider = new Swiper(".blog-slider", {
     slidesPerView: "auto",
     spaceBetween: 18,
+    navigation: {
+      nextEl: ".swiper-button-next.btn-blog",
+      prevEl: ".swiper-button-prev.btn-blog",
+    },
+  });
+
+  var swiperProductSlider = new Swiper(".productSlider", {
+    slidesPerView: 5,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: ".swiper-button-next.next-btn",
+      prevEl: ".swiper-button-prev.prev-btn",
+    },
+  });
+
+  var swiperCategorySliderChair = new Swiper("#category-chair", {   
+    slidesPerView: 4,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: ".swiper-button-next.next-btn-style2",
+      prevEl: ".swiper-button-prev.prev-btn-style2",
+    },
+  });
+
+  var swiperCategorySlider = new Swiper("#category-sport", {   
+    slidesPerView: 4,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: ".swiper-button-next.next-btn-style2.sport",
+      prevEl: ".swiper-button-prev.prev-btn-style2.sport",
+    },
+  });
+
+  var swiperVideo = new Swiper(".video-slider", {
+    slidesPerView: 3,
+    spaceBetween: 24,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
   });
 
-  var swiperProductSlider = new Swiper(".productSlider", {
-    slidesPerView: 5,
-    spaceBetween: 30,
+  var swiperBlogNews = new Swiper(".blog-news-slider", {
+    slidesPerView: 3,
+    spaceBetween: 24,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,

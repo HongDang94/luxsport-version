d3353cea3dd40a2b257a70ff32160507e35326d0
hong.dt <hong.thdang@gmail.com>
2022-07-29 23:38:39 +0700

    update

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,32 +1,17 @@
 jQuery(document).ready(function ($) {
     "use strict";
-    function updSwiperNumericPagination() {
-        this.el.querySelector(".swiper-counter").innerHTML = '<span class="count">' + (this.realIndex + 1) + '</span>/<span class="total">' + this.el.slidesQuantity + "</span>";
-      }
-      
-      $(".detail-product-slider").each(function() {
-        // Getting slides quantity before slider clones them
-        this.slidesQuantity = this.querySelectorAll(".swiper-slide").length;
-      
-        // Swiper initialization
-        new Swiper(this, {
-          speed: 800,
-          loop: false,
-          pagination: {
-            el: this.querySelector(".swiper-pagination")
-          },
-          on: {
-            init: updSwiperNumericPagination,
-            slideChange: updSwiperNumericPagination
-          }
-        });
-      });
-
+    var slides_count = document.getElementsByClassName("swiper-slide");
        var swiper = new Swiper(".slider-thumb", {
         spaceBetween: 16,
         slidesPerView: 6,
         freeMode: true,
         watchSlidesProgress: true,
+        on: {
+          slideChange: function(){
+              var offer = document.querySelector('#numberSlides');
+              offer.innerHTML = (this.activeIndex +  1) + '/' + slides_count.length + 'Offers';
+          }
+        }
       });
       var swiper2 = new Swiper(".detail-product-slider", {
         spaceBetween: 10,
@@ -39,7 +24,5 @@ jQuery(document).ready(function ($) {
         },
       });
 
-      
-      
   });
   

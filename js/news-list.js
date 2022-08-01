4bcc008ec560f8033e88fa0371b14bcb2ad44eb3
HongDang94 <hong.thdang@gmail.com>
2022-08-01 20:50:18 +0700

    Update news-list

--- js/news-list.js
+++ js/news-list.js
@@ -1,35 +1,6 @@
 jQuery(document).ready(function ($) {
   "use strict";
 
-  // var swiper = new Swiper(".slider-thumb", {
-  //   spaceBetween: 8,
-  //   slidesPerView: "auto",
-  //   freeMode: true,
-  //   watchSlidesVisibility: true,
-  //   watchSlidesProgress: true,
-  //   slideToClickedSlide: true,
-  // });
-
-  // var swiper2 = new Swiper(".news-list", {
-  //   spaceBetween: 10,
-  //   effect: "fade",
-  //   autoHeight: true,
-
-  //   thumbs: {
-  //     swiper: swiper,
-  //   },
-  // });
-
-  // swiper2.on("slideChange", function () {
-  //   var idx = tabContent.activeIndex;
-  //   swiper.slideTo(idx, 500);
-  // });
-
-  // $(".slider-thumb .swiper-wrapper a").click(function () {
-  //   $(this).addClass("selected");
-  //   $(this).siblings().removeClass("selected");
-  // });
-
   var tabTit = new Swiper(".slider-thumb", {
     spaceBetween: 8,
     slidesPerView: "auto",
@@ -42,6 +13,8 @@ jQuery(document).ready(function ($) {
     spaceBetween: 10,
     autoHeight: true,
     effect: "fade",
+    observer: true,
+    observeParents: true,
     thumbs: {
       swiper: tabTit,
     },

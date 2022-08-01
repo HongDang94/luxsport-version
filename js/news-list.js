29a5084fc3a7ae04074fba1c292c6a9d4438c1a5
Đặng Thị Hồng <hong.thdang@gmail.com>
2022-08-01 13:06:17 +0700

    update

--- js/news-list.js
+++ js/news-list.js
@@ -1,24 +1,24 @@
 jQuery(document).ready(function ($) {
   "use strict";
 
-  var swiper = new Swiper(".slider-thumb", {
-    spaceBetween: 8,
-    slidesPerView: "auto",
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    slideToClickedSlide: true,
-  });
+  // var swiper = new Swiper(".slider-thumb", {
+  //   spaceBetween: 8,
+  //   slidesPerView: "auto",
+  //   freeMode: true,
+  //   watchSlidesVisibility: true,
+  //   watchSlidesProgress: true,
+  //   slideToClickedSlide: true,
+  // });
 
-  var swiper2 = new Swiper(".news-list", {
-    spaceBetween: 10,
-    effect: "fade",
-    autoHeight: true,
+  // var swiper2 = new Swiper(".news-list", {
+  //   spaceBetween: 10,
+  //   effect: "fade",
+  //   autoHeight: true,
 
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  //   thumbs: {
+  //     swiper: swiper,
+  //   },
+  // });
 
   // swiper2.on("slideChange", function () {
   //   var idx = tabContent.activeIndex;
@@ -30,5 +30,28 @@ jQuery(document).ready(function ($) {
   //   $(this).siblings().removeClass("selected");
   // });
 
-
+  var tabTit = new Swiper(".slider-thumb", {
+    spaceBetween: 8,
+    slidesPerView: "auto",
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    slideToClickedSlide: true,
+  });
+  var tabContent = new Swiper(".news-list", {
+    spaceBetween: 10,
+    autoHeight: true,
+    effect: "fade",
+    thumbs: {
+      swiper: tabTit,
+    },
+  });
+  tabContent.on("slideChange", function () {
+    var idx = tabContent.activeIndex;
+    tabTit.slideTo(idx, 500);
+  });
+  $(".slider-thumb .swiper-wrapper a").click(function () {
+    $(this).addClass("selected");
+    $(this).siblings().removeClass("selected");
+  });
 });

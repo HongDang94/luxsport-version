87c4c310620da8a4564ec1f3310f79d87db058a6
HongDang94 <hong.thdang@gmail.com>
2022-08-01 22:59:12 +0700

    Update gio hang

---
+++ js/common.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function ($) {
+    "use strict";
+  
+    var openMenu = function () {
+      $(".btn_menu").on("click", function () {
+        $(".gnb").show();
+        $("body").addClass("show_menu");
+        $(".gnb")
+          .stop()
+          .animate({ left: "0" }, 600, "easeInOutExpo", function () {});
+      });
+    };
+  
+    var closeMenu = function () {
+      $(".btn_close").on("click", function () {
+        $("body").removeClass("show_menu");
+        $(".gnb")
+          .stop()
+          .animate({ left: "-100%" }, 600, "easeInOutExpo", function () {
+            //scrollLock(false);
+            $(".gnb").hide();
+          });
+      });
+    };
+  
+    openMenu();
+    closeMenu();
+  });
+  
